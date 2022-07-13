dad31ec062a1ace4c10a07e34b1a6898bda45187
lakshya singh chouhan <lakshyasinghchouhan2509@gmail.com>
2022-07-13 13:27:50 -0400

    complete the result section and install html-react-parser

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Parser from "html-react-parser"; 
 export default function SearchResults({results}) {
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
         <p className="text-gray-600 text-sm mb-5 mt-3"> About {results.searchInformation.formattedTotalResults} results in {results.searchInformation.formattedSearchTime} secs</p>
+      {results.items.map((result)=>(
+        <div key={result.link} className="max-w-xl mb-8">
+          <div className="group">
+          <a href={result.link} className="text-sm truncate">{result.formattedUrl}</a>
+          <a href={result.link} className="group-hover:underline text-blue-800">
+            <h2 className="truncate text-xl font-medium ">{result.title}</h2>
+          </a>
+          </div>
+          <p className="text-gray-600">{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>  
   )
 }
--- pages/search.js
+++ pages/search.js
@@ -24,7 +24,7 @@ export default function search({results}) {
   )
 }
 export async function getServerSideProps(context){
-    const mockData= true
+    const mockData= true;
     const data= mockData ? Response :await fetch( 
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${context.query.searchType && "&searchType=image"}
     `).then((response)=>response.json());
